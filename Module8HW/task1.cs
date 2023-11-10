7be0c871497fabb22c83e4311e62d2b8bbd219ca
Ser1kkhan <serikhana78@gmail.com>
2023-11-10 19:52:03 +0600

    Добавьте файлы проекта.

---
+++ Module8HW/task1.cs
@@ -0,0 +1,73 @@
+﻿//using System;
+
+//class SquaringArray
+//{
+//    private int[] arr;
+
+//    public SquaringArray(int size)
+//    {
+//        arr = new int[size];
+//    }
+
+//    public int this[int index]
+//    {
+//        get
+//        {
+//            if (index >= 0 && index < arr.Length)
+//            {
+//                return arr[index];
+//            }
+//            else
+//            {
+//                throw new IndexOutOfRangeException("Index is out of range.");
+//            }
+//        }
+//        set
+//        {
+//            if (index >= 0 && index < arr.Length)
+//            {
+//                arr[index] = value * value;
+//            }
+//            else
+//            {
+//                throw new IndexOutOfRangeException("Index is out of range.");
+//            }
+//        }
+//    }
+
+//    public void DisplayArray()
+//    {
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            Console.WriteLine($"Element at index {i}: {arr[i]}");
+//        }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        SquaringArray squares = new SquaringArray(5);
+
+//        squares[0] = 2; // Sets value at index 0 to 4
+//        squares[1] = 3; // Sets value at index 1 to 9
+
+//        Console.WriteLine($"Value at index 0: {squares[0]}"); // Retrieves the squared value (4)
+//        Console.WriteLine($"Value at index 1: {squares[1]}"); // Retrieves the squared value (9)
+
+//        squares.DisplayArray(); // Displays all elements in the array
+
+//        // Accessing an out-of-range index will throw an exception
+//        try
+//        {
+//            squares[10] = 5; // Throws an IndexOutOfRangeException
+//        }
+//        catch (IndexOutOfRangeException ex)
+//        {
+//            Console.WriteLine(ex.Message);
+//        }
+
+//        Console.ReadKey();
+//    }
+//}
---
+++ Module8HW/task2.cs
@@ -0,0 +1,63 @@
+﻿//using System;
+
+//class UtilityPaymentCalculator
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Utility Payment Calculator");
+
+//        // Input room area, number of residents, season, and discounts
+//        Console.Write("Enter room area (in square meters): ");
+//        double roomArea = double.Parse(Console.ReadLine());
+
+//        Console.Write("Enter the number of residents: ");
+//        int numberOfResidents = int.Parse(Console.ReadLine());
+
+//        Console.Write("Enter the season (1 for autumn, 2 for winter): ");
+//        int season = int.Parse(Console.ReadLine());
+
+//        Console.Write("Is the resident a veteran of labor? (y/n): ");
+//        bool isVeteranLabor = Console.ReadLine().ToLower() == "y";
+
+//        Console.Write("Is the resident a war veteran? (y/n): ");
+//        bool isWarVeteran = Console.ReadLine().ToLower() == "y";
+
+//        // Utility tariffs
+//        double heatingTariff = season == 2 ? 2.0 : 1.5; // Higher tariff in winter
+//        double waterTariff = 5.0;
+//        double gasTariff = 4.0;
+//        double repairTariff = 1.0;
+
+//        // Calculate costs
+//        double heatingCost = roomArea * heatingTariff;
+//        double waterCost = numberOfResidents * waterTariff;
+//        double gasCost = numberOfResidents * gasTariff;
+//        double repairCost = roomArea * repairTariff;
+
+//        double totalCost = heatingCost + waterCost + gasCost + repairCost;
+
+//        // Apply discounts
+//        double discount = 0.0;
+//        if (isVeteranLabor) discount += totalCost * 0.3;
+//        if (isWarVeteran) discount += totalCost * 0.5;
+
+//        double discountedTotal = totalCost - discount;
+
+//        // Display the results in a table
+//        Console.WriteLine("\nUtility Payments:");
+//        Console.WriteLine("--------------------------------------------------------");
+//        Console.WriteLine("| Type            | Amount     | Discount    | Total      |");
+//        Console.WriteLine("--------------------------------------------------------");
+//        Console.WriteLine($"| Heating         | ${heatingCost:F2}  |            | ${heatingCost:F2}  |");
+//        Console.WriteLine($"| Water           | ${waterCost:F2}   |            | ${waterCost:F2}   |");
+//        Console.WriteLine($"| Gas             | ${gasCost:F2}     |            | ${gasCost:F2}     |");
+//        Console.WriteLine($"| Repair          | ${repairCost:F2}  |            | ${repairCost:F2}  |");
+//        Console.WriteLine("--------------------------------------------------------");
+//        Console.WriteLine($"| Total           | ${totalCost:F2}  | ${discount:F2} | ${discountedTotal:F2} |");
+//        Console.WriteLine("--------------------------------------------------------");
+
+//        Console.WriteLine($"Total utility payment: ${discountedTotal:F2}");
+
+//        Console.ReadKey();
+//    }
+//}
